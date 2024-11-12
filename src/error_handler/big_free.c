b0cc376993867ffb6205f5360459a238be005b22
Basil Claeys <bclaeys@a1-r3-p10.s19.be>
2024-11-12 16:37:27 +0100

    Problem with the libft Makefile.

--- src/error_handler/big_free.c
+++ src/error_handler/big_free.c
@@ -24,14 +24,15 @@ void	free_lexer(t_token_node *first_node_lexer)
 	{
 		tmp = current;
 		current = current->next;
-		// Volgende free loopt soms fout, "attempted double free", waarom?
+		// Volgendes free loopt soms fout, "attempted double free", waarom?
 		// Ik kan de fouten ook niet herhalen als ze voorkomen. 
 		if (tmp->token)
 			free(tmp->token);
 		if (tmp)
 			free(tmp);
 	}
-	first_node_lexer = NULL;
+	/* first_node_lexer->token = NULL; */
+	/* first_node_lexer = NULL; */
 }
 void	*free_var_data(t_var_data *var_data)
 {
